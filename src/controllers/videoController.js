b7c53058c7600f96fe8502995cf2e41abd3c9067
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-10-08 22:07:15 +0900

    WETUBE

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+// render(파일명, 보내고자 하는 변수)
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
