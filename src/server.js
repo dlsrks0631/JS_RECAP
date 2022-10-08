58d4af00f7ae95ac1d9a699127078fcd54bceafb
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-10-08 21:39:16 +0900

    WETUBE

--- src/server.js
+++ src/server.js
@@ -9,8 +9,10 @@ const PORT = 4000;
 const app = express(); // express-function -> express application을 생성
 
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
