c04b84cc493b09657fc7fe46d2778d7c7ee6df2c
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-12-06 22:20:36 +0900

    wetube

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controllers/videoController";
+import { 
+  watch,
+  getUpload,
+  getEdit,
+  postEdit,
+  postUpload
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
@@ -7,5 +13,9 @@ videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 //== videoRouter.get("/:id(\\d+)/edit", getEdit);
 //== videoRouter.post("/:id(\\d+)/edit", postEdit);
+
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+// videoRouter.get("/upload", getUpload);
+// videoRouter.post("/upload", postUpload);
 
 export default videoRouter;
