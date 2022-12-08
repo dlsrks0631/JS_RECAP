e488bc1e426eced146513706e3abee5ff081873b
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-12-09 00:19:50 +0900

    wetube

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -4,7 +4,7 @@ import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import "./db";
+
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
+
 
 const app = express(); // express-function -> express application을 생성
 
@@ -19,12 +20,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server Listening on port http://localhost:${PORT}`);
-
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here" });
-};
-
-app.listen(4000, handleListening);
-app.get("/login", handleLogin);
+export default app;
