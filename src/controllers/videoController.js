0abb4f0b0e390d65eb07de248f6290fec17a684d
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-12-09 15:49:24 +0900

    wetube

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,11 @@
 import Video from "../models/Video";
 
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    console.log("errors", error);
-    console.log("videos", videos);
-  })
-  return res.render("home", { pageTitle: "Home", videos: [] });
+//Video.find({}, (error,videos) => {}
+
+export const home = async(req, res) => {
+    const videos = await Video.find({})
+    return res.render("home", { pageTitle: "Home", videos });
 };
 
 // render(파일명, 보내고자 하는 변수)
@@ -31,6 +30,17 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   // req.body -> input에서 데이터를 받아오기 위해 사용
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title:title,
+    description:description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map(word=> `#${word}`),
+    meta: {
+      views:0,
+      rating:0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
